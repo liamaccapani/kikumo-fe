716cb2e64b3727483f74ec51c451ad8ab5837a8e
Lia Maccapani <lia.maccapani@gmail.com>
2021-11-30 12:23:35 +0100

    getting to profile (without state)

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+
+
+const Profile = ({ history, location, match }) => {
+    return( 
+        <h1>PROFILE</h1>
+    )
+}
+
+export default Profile
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,34 +1,26 @@
 import { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
-
+import { useDispatch, useSelector  } from "react-redux";
+import { setUserInfo } from "../../redux/actions";
 
 const Register = ({ history, location, match }) => {
-  const [newUser, setNewUser] = useState({
-    name: '',
-    surname: '',
-    email: '',
-    password: '',
-    role: ['Client', 'Therapist']
-  })
-  // const [name, setName] = useState('')
+  const [name, setName] = useState('')
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleInput = (e, propertyName) => {
-    setNewUser({
-      ...newUser,
-      [propertyName]: e.target.value
-    })
-  }
+  // const dispatch = useDispatch()
 
   const register = async (e) => {
+    // const { name, surname, email, password } = newUser
     e.preventDefault()
-    // const newUser = { name, surname, email, password }
     try {
-      const response = await fetch(process.env.REACT_APP_DEV_API_BE + "/users/register", {
-        method: "POST", 
+      const response = await fetch(process.env.REACT_APP_DEV_API_BE + "/clients/register", {
+        method: "POST",
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(newUser),
+        body: JSON.stringify({ name, surname, email, password }),
       })
       if(response.ok) {
         const data = await response.json()
@@ -41,6 +33,24 @@ const Register = ({ history, location, match }) => {
     }
   }
 
+  // const getUserInfo = async () => {
+  //   try {
+  //     console.log("here 0")
+  //     const response = await fetch(process.env.REACT_APP_DEV_API_BE + "/clients/me")
+  //     console.log("here 1")
+  //     if(response.ok){
+  //       const {data} = await response.json()
+  //       console.log("here 2")
+  //       if(data){
+  //         dispatch(setUserInfo(data))
+  //         history.push("/profile")
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
   return (
     <>
       <h1> REGISTER </h1>
@@ -51,32 +61,36 @@ const Register = ({ history, location, match }) => {
             size="sm"
             type="text"
             placeholder=""
-            value={newUser.name}
-            onChange={(e) => handleInput(e, 'name')}
+            value={name}
+            onChange={(e)=> setName(e.target.value)}
+            // onChange={(e) => handleInput(e, 'name')}
           />
           <Form.Label>Your Surname</Form.Label>
           <Form.Control
             size="sm"
             type="text"
             placeholder=""
-            value={newUser.surname}
-            onChange={(e) => handleInput(e, 'surname')}
+            value={surname}
+            onChange={(e)=> setSurname(e.target.value)}
+            // onChange={(e) => handleInput(e, 'surname')}
           />
           <Form.Label>Your email</Form.Label>
           <Form.Control
             size="sm"
             type="email"
             placeholder=""
-            value={newUser.email}
-            onChange={(e) => handleInput(e, 'email')}
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            // onChange={(e) => handleInput(e, 'email')}
           />
           <Form.Label>Choose Password</Form.Label>
           <Form.Control
             size="sm"
             type="password"
             placeholder=""
-            value={newUser.password}
-            onChange={(e) => handleInput(e, 'password')}
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            // onChange={(e) => handleInput(e, 'password')}
           />
           <Form.Label>Register as:</Form.Label>
           <Form.Check
@@ -84,19 +98,21 @@ const Register = ({ history, location, match }) => {
             label="Client"
             value="Client"
             name="role"
-            // checked={newUser.role} // true or false
-            onChange={(e) => handleInput(e, 'role')}      
+            // checked={role} // true or false
+            // onChange={(e) => handleInput(e, 'role')}
+            // onChange={(e)=>}     
           />
           <Form.Check
             type="checkbox"
             label="Therapist"
             value="Therapist"
             name="role"
-            onChange={(e) => handleInput(e, 'role')}      
+            // onChange={(e) => handleInput(e, 'role')}
+            // onChange={(e)=>}
           />
         </Form.Group>
         <Button type="submit" variant="success">
-          Sign In
+          Register
         </Button>
       </Form>
     </>
