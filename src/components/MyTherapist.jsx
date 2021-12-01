deedc73cfcc3fba8a2beabe01f1f22f8d38861f6
Lia Maccapani <lia.maccapani@gmail.com>
2021-12-01 12:56:32 +0100

    getting therapist (need for ID!!)

--- src/components/MyTherapist.jsx
+++ src/components/MyTherapist.jsx
@@ -3,8 +3,8 @@ import { Card } from "react-bootstrap";
 const MyTherapist = ({showTherapistDetails, therapist}) => {
     return(
         <div onClick={()=> showTherapistDetails()}>
-           <Card className="d-flex justify-content-start">
-               <img src={therapist.avatar}/>
+           <Card className="d-flex flex-row">
+               <img height="80" width="80"src={therapist.avatar}/>
                <div>{therapist.name} {therapist.surname}</div>
            </Card> 
         </div>
