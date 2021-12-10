95e4940c5e8d23685850079bdd5104864d32f875
Lia Maccapani <lia.maccapani@gmail.com>
2021-12-10 20:05:20 +0100

    calendar experiment

--- src/pages/Blank.jsx
+++ src/pages/Blank.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 import format from "date-fns/format";
-import parseISO from "date-fns/parseISO"
+import parseISO from "date-fns/parseISO";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 const Blank = ({ history, location, match }) => {
   const [myData, setMyData] = useState({});
   const [myAppointments, setMyAppointments] = useState([]);
-  const [myTherapists, setMyTherapists] = useState([])
-  
+  const [myTherapists, setMyTherapists] = useState([]);
+  const [currentEvents, setCurrentEvents] = useState([])
+
   const getMe = async () => {
     const token = localStorage.getItem("accessToken");
     try {
       const response = await fetch(
-        process.env.REACT_APP_DEV_API_BE + "/clients/me",
+        process.env.REACT_APP_DEV_API_BE + "/therapists/me",
         {
           headers: {
             Authorization: "Bearer " + token,
@@ -27,11 +32,12 @@ const Blank = ({ history, location, match }) => {
       console.log(error);
     }
   };
+
   const getMyAppointments = async () => {
     const token = localStorage.getItem("accessToken");
     try {
       const response = await fetch(
-        process.env.REACT_APP_DEV_API_BE + "/sessions/clients",
+        process.env.REACT_APP_DEV_API_BE + "/sessions",
         {
           headers: {
             Authorization: "Bearer " + token,
@@ -41,108 +47,72 @@ const Blank = ({ history, location, match }) => {
       if (response.ok) {
         const appointments = await response.json();
         setMyAppointments(appointments);
-        appointments.map(appointment => {
-          console.log(appointment)
-          console.log(myTherapists)
-          // const index = myTherapists.findIndex((therapist) => (therapist._id !== appointment.therapistId) === -1 )
-          // if(index === -1) {
-          //   let newMyTherapists = [...myTherapists]
-          //   newMyTherapists.push(therapist)
-          //   setMyTherapists(newMyTherapists)
-          // }
-        });
-        // console.log("APPOINTMENTS", appointments);
-        // console.log("THERAPISTS", myTherapists)
+        console.log(myAppointments)
       }
     } catch (error) {
       console.log(error);
     }
   };
 
+  // const handleDateSelect = (selectInfo) => {
+  //   let title = prompt('Please enter a new title for your event')
+  //   // let calendarApi = selectInfo.view.calendar
+
+  //   // calendarApi.unselect() // clear date selection
+
+  //   // if (title) {
+  //   //   calendarApi.addEvent({
+  //   //     id: createEventId(),
+  //   //     title,
+  //   //     start: selectInfo.startStr,
+  //   //     end: selectInfo.endStr,
+  //   //     allDay: selectInfo.allDay
+  //   //   })
+  //   // }
+  // }
+  // const handleEventClick = (clickInfo) => {
+  //   if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+  //     clickInfo.event.remove()
+  //   }
+  // }
+  // const handleEvents = (events) => {
+  //   setCurrentEvents(events)
+  // }
+  // const renderEventContent = (eventInfo) => {
+  //   return (
+  //     <>
+  //       <b>{eventInfo.timeText}</b>
+  //       <i>{eventInfo.event.title}</i>
+  //     </>
+  //   )
+  // }
+  
   useEffect(() => {
     getMe();
     getMyAppointments();
   }, []);
+
+
   return (
-    <div>
-      {myAppointments.map((appointment) => {
-        return (
-          <>
-            <div>{format(new Date(2021, 7, 12), "yyyy-MM-dd")}</div>
-            {/* {console.log(myTherapists)} */}
-            <div>{format(parseISO(appointment.startDate), "yyyy-MM-dd")}</div>
-          </>
-        );
-      })}
-    </div>
-  );
-};
+    <FullCalendar 
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      }}
+      initialView="timeGridWeek"
+      editable={true}
+      selectable={true}
+      businessHours={true}
+      // select={()=>handleDateSelect()}
+      // eventContent={renderEventContent()}
+      // eventClick={handleEventClick()}
+      // eventsSet={handleEvents()}
+    />
+    )
+}
 
 export default Blank;
 
 
-{
-  /* <Container>
-  <Row>
-    <Col
-      md={12}
-      lg={6}
-      className="profile-col"
-      style={{ backgroundColor: "pink" }}
-    >
-      <Container>
-        <div>{myData.name}</div>
-        <div>{myData.surname}</div>
-        <div>
-          <img alt="avatar" src={myData.avatar} height="80" width="80" />
-        </div>
-      </Container>
-    </Col>
-    <Col className="utilities-col" style={{ backgroundColor: "orange" }}>
-      <Card body>Search Therapist</Card>
-
-      {!showAppointments && (
-        <Card body id="appointments" onClick={() => showAppointmentsDetails()}>
-          Appointments
-        </Card>
-      )}
-      {showAppointments && (
-        <MyAppointments
-          onClick={() => showAppointmentsDetails()}
-          showAppointmentsDetails={showAppointmentsDetails}
-          appointments={myData.appointments}
-        />
-      )}
-
-      {!showTherapist && (
-        <Card body onClick={() => showTherapistDetails()}>
-          My Therapist
-        </Card>
-      )}
-      {showTherapist && (
-        <MyTherapist
-          onClick={() => showTherapistDetails()}
-          showTherapistDetails={showTherapistDetails}
-          therapist={myData.therapist}
-        />
-      )}
-    </Col>
-  </Row>
-</Container>; */
-}
-// // Fx to pass down to the cards
-// const showDetails = () => {
-//   if(id === "appointments"){
-//     setShowAppointments(!showAppointments)
-//   } else {
-//     showTherapist(!setShowTherapist)
-//   }
-// }
-
-// const showAppointmentsDetails = () => {
-//   setShowAppointments(!showAppointments);
-// };
-
-// const showTherapistDetails = () => {
-//   setShowTherapist(!showTherapist);
-// };
