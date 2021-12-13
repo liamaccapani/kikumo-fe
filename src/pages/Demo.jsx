e4cf84b8b6784db65b02bccb994067619d2a1660
Lia Maccapani <lia.maccapani@gmail.com>
2021-12-13 14:56:54 +0100

    displaying events on refresh

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -6,22 +6,20 @@ import interactionPlugin from "@fullcalendar/interaction";
 
 export default class DemoApp extends React.Component {
   state = {
-    sessions: [],
+    sessions: [
+      {
+        start: "",
+        end: "",
+      },
+    ],
     start: "",
     end: "",
     filledIn: false,
   };
 
   componentDidMount() {
-      this.getAllSessions()
+    this.getAllSessions();
   }
-
-//   onEventAdded = (event) => {
-//     const calendarRef = useRef();
-//     const api = calendarRef.current.getApi();
-//     api.addEvent(event);
-//   };
-
 
   render() {
     return (
@@ -31,78 +29,96 @@ export default class DemoApp extends React.Component {
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             initialView="timeGridWeek"
             headerToolbar={{
-                left: "prev,next today",
-                center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay",
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
             }}
+            weekends={false}
             editable={true}
             selectable={true}
             dayMaxEvents={true}
             aspectRatio={6}
             height={600}
             select={this.handleTimeSelection}
-            events={this.state.sessions}  // this renderes the event objects in the calendar
-            // eventContent={this.renderEventContent}
+            // eventsSet={this.handleEvents}
+            eventAdd={this.createSession}
+            events={this.state.sessions} // this renders the event objects in the calendar
           />
         </div>
-        {this.state.filledIn === true ?
-         <div>
-             Confirm? 
-             <button onClick={this.createSession}>Yes</button>
-         </div> :
-          null
-        }
+        {this.state.filledIn === true ? (
+          <div>
+            Confirm?
+            <button onClick={this.createSession}>Yes</button>
+          </div>
+        ) : null}
       </div>
     );
   }
 
-//   renderEventContent = (eventInfo) => {
-//     return (
-//       <>
-//         <b>{eventInfo.timeText}</b>
-//         <i>{eventInfo.event.title}</i>
-//       </>
-//     )
-//   }
-
   handleTimeSelection = (info) => {
+    // this.setState({
+    //   sessions: [
+    //     {
+    //       start: info.startStr,
+    //       end: info.endStr,
+    //     },
+    //   ],
+    //   filledIn: true
+    // });
+
     this.setState({
+      sessions: [
+        {
+          start: info.startStr,
+          end: info.endStr,
+        },
+      ],
       start: info.startStr,
       end: info.endStr,
       filledIn: true,
     });
     console.log(info);
   };
-  
+
   toggleFilledIn = () => {
-      this.setState({
-          filledIn: false
-      })
-  }
+    this.setState({
+      sessions: [
+        {
+          filledIn: false,
+        },
+      ],
+    });
+  };
+
+  // handleEvents = (events) => {
+  //   this.setState({
+  //     sessions: events
+  //   })
+  // }
 
   createSession = async (e) => {
     e.preventDefault();
     const token = localStorage.getItem("accessToken");
     try {
-    const response = await fetch(
+      const response = await fetch(
         process.env.REACT_APP_DEV_API_BE + "/test/addEvent",
         {
-          method: 'POST',
+          method: "POST",
           headers: {
             Authorization: "Bearer " + token,
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({
-              start: this.state.start,
-              end: this.state.end
-          })
+            start: this.state.start,
+            end: this.state.end,
+          }),
         }
       );
-      if(response.ok){
+      if (response.ok) {
         const data = await response.json();
-        console.log("POST", data)
-        this.state.sessions.push(data)
-        this.toggleFilledIn()
+        console.log("POST", data);
+        this.state.sessions.push(data);
+        this.toggleFilledIn();
       }
     } catch (error) {
       console.log(error);
@@ -120,15 +136,15 @@ export default class DemoApp extends React.Component {
           },
         }
       );
-      if(response.ok){
-        const data = await response.json()
+      if (response.ok) {
+        const data = await response.json();
         this.setState({
-            sessions: [data]
+            sessions: [...data]
         })
         console.log("GET", data)
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 }
