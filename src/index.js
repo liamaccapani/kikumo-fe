212904aa869d24f15ddf48a12d776bb11044e653
Lia Maccapani <lia.maccapani@gmail.com>
2021-12-18 18:09:29 +0100

    cleared readme and index.js test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-// test!! \\
-import "react-datetime/css/react-datetime.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
